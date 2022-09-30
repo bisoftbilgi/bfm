e2d8be86b9b4ba87aff799aefc9bccc46511fa43
serdar güler <serdar.guler@bisoft.com.tr>
2022-09-30 11:25:56 +0300

    configuration file value trimming added

--- src/main/java/com/bisoft/bfm/BfmApplication.java
+++ src/main/java/com/bisoft/bfm/BfmApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
@@ -22,6 +23,14 @@ public class BfmApplication implements CommandLineRunner {
 		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
 		taskScheduler.setPoolSize(10);
 		return  taskScheduler;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer createPropertyConfigurer()
+	{
+		PropertySourcesPlaceholderConfigurer propertyConfigurer = new PropertySourcesPlaceholderConfigurer();
+		propertyConfigurer.setTrimValues(true);
+		return propertyConfigurer;
 	}
 
 
