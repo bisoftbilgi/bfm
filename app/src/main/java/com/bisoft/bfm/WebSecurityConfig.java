5e322550b9cd821d35467a30963036bc715ec2b4
sdtbsmc <sedatbasmaci@gmail.com>
2024-07-04 14:33:37 +0000

    CORS BASIC authentication header set

--- app/src/main/java/com/bisoft/bfm/WebSecurityConfig.java
+++ app/src/main/java/com/bisoft/bfm/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
